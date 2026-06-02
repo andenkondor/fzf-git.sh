35252f4158df62f6e34ffd5d7ffcb41bd9be6d73
rolfi <t.rolfsmeier@live.de>
2026-06-02 20:51:44 +0200

    feat: --exact and --no-sort activated

--- fzf-git.sh
+++ fzf-git.sh
@@ -177,7 +177,8 @@ else
       --border-label-pos 2 \
       --color 'label:blue' \
       --preview-window 'right,50%' --preview-border line \
-      --bind 'ctrl-/:change-preview-window(down,50%|hidden|)' "$@"
+      --bind 'ctrl-/:change-preview-window(down,50%|hidden|)' \
+      --exact --no-sort "$@"
   }
 fi
 
@@ -352,14 +353,24 @@ _fzf_git_each_ref() {
 
 _fzf_git_worktrees() {
   _fzf_git_check || return
-  git worktree list | _fzf_git_fzf \
+  git worktree list | awk '{
+    path = $1
+    if ($3 ~ /^\[/) {
+      gsub(/^\[|\]$/, "", $3)
+      branch = $3
+    } else {
+      branch = "(detached)"
+    }
+    print path "\t" branch
+  }' | _fzf_git_fzf \
     --border-label '🌴 Worktrees ' \
     --header 'CTRL-X (remove worktree)' \
     --bind 'ctrl-x:reload(git worktree remove {1} > /dev/null; git worktree list)' \
+    --with-nth 1,2 \
     --preview "
       git -c color.status=$(__fzf_git_color .) -C {1} status --short --branch
       echo
-      git log --oneline --graph --date=short --color=$(__fzf_git_color .) --pretty='format:%C(auto)%cd %h%d %s' {2} --
+      git -C {1} log --oneline --graph --date=short --color=$(__fzf_git_color .) --pretty='format:%C(auto)%cd %h%d %s' --
     " "$@" |
   awk '{print $1}'
 }
